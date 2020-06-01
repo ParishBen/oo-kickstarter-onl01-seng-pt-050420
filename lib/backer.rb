639930b4b08a86084c365982a72620aa0a515a14
Ben Parish <parishben13@gmail.com>
2020-06-01 20:48:50 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,23 +6,8 @@ class Backer
     @backed_projects=[]
    end
   def back_project(project)
-    prjct = Project.new(prjct) 
-  self.backed_projects << Project.new(prjct)
-    prjct.backers = self
+    @backed_projects << project
+    project.backers << self
   end
 end
 
-class Backer
-  attr_reader :name, :backed_projects
-
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
-
-  def back_project(project)
-    backed_projects << project
-    project.backers << self
-  end
-
-end    
